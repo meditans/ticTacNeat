400ad96c5d9821ca8b4c9af61043642f2a0b8cff
meditans <meditans@gmail.com>
2016-04-12 00:55:22 +0200

    Stub of main functionality

--- app/Main.hs
+++ app/Main.hs
@@ -6,7 +6,23 @@
 module Main where
 
 -- import TicTacToe
--- import FightTrainMethod
+import FightTrainMethod
+
+import Control.Monad.Random
+import Neet
 
 main :: IO ()
-main = putStrLn "hi"
+main = do
+  trainedPop <- evalRandIO $ trainN (fightTrainMethod _what 3) 100 ticTacPopulation
+  putStrLn "Here's the best player after this training:"
+  renderGenome (popBOrg trainedPop)
+
+ticTacPopulation :: Population
+ticTacPopulation = newPop 100 PS { psSize = 100
+                                 , psInputs = 9
+                                 , psOutputs = 9
+                                 , psParams = defParams
+                                 , sparse = Nothing
+                                 , psStrategy = Nothing
+                                 }
+
