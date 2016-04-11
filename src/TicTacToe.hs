a07f190fecbce77def3b7e8a25c79763f50daaf2
Francesco Gazzetta <fgaz@users.noreply.github.com>
2016-04-11 22:03:46 +0200

    checked & linted

--- src/TicTacToe.hs
+++ src/TicTacToe.hs
@@ -3,6 +3,7 @@ module TicTacToe where
 import Data.Map
 import Prelude hiding (lookup)
 import Data.Monoid
+import Data.Maybe (isJust)
 
 data Genome = Genome --test
 
@@ -13,7 +14,7 @@ data Player = X | O deriving (Enum, Eq, Show)
 type Board = Map (Int, Int) Player
 
 move :: Player -> (Int,Int) -> Board -> Maybe Board
-move p xy b | lookup xy b /= Nothing = Nothing --se c'è già qualcosa la mossa non è valida
+move p xy b | isJust $ lookup xy b = Nothing --se c'è già qualcosa la mossa non è valida
             | otherwise = Just $ insert xy p b
 
 getWinner :: Board -> Maybe Player
@@ -31,9 +32,9 @@ getWinner b = getFirst (r0 <> r1 <> r2 <> c0 <> c1 <> c2 <> d0 <> d1)
                       | otherwise = First Nothing
 
 play :: (Board -> (Int,Int)) -> (Board -> (Int,Int)) -> (Double, Double)
-play p1 p2 = play' X p1 p2 empty
+play = play' X empty
   where
-    play' turn p1 p2 board = case winner of Nothing -> scores
+    play' turn board p1 p2 = case winner of Nothing -> scores
                                             Just X -> (1,-1)
                                             Just O -> (-1,1)
       where
@@ -41,12 +42,12 @@ play p1 p2 = play' X p1 p2 empty
         moveResult = case turn of X -> move X (p1 board) board
                                   O -> move O (p2 board) board
         scores = case moveResult of Nothing -> failed turn
-                                    Just b -> play' (succ turn) p1 p2 b
+                                    Just b -> play' (succ turn) b p1 p2
         failed X = (-2,0)
         failed O = (0,-2)
 
 genomeToFunctionThatTakesABoardAndReturnsAMove :: Genome -> (Board -> (Int,Int))
-genomeToFunctionThatTakesABoardAndReturnsAMove = undefined --esecuzione della rete neurale
+genomeToFunctionThatTakesABoardAndReturnsAMove = undefined --TODO esecuzione della rete neurale
 
 scoreTicTacToe :: ScoringFunction
 scoreTicTacToe g1 g2 = play
