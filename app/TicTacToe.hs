7a33c228f52a278b715f5114c440cceac46f5f7e
meditans <meditans@gmail.com>
2016-04-12 14:10:16 +0200

    Board Position is Int, implemented abstraction
    The board position is represented internally as an Int now, to smooth
    the communication with the neural network representation.
    
    The abstractGenome function turns a genome in a function which chooses a
    move on the board.

--- app/TicTacToe.hs
+++ app/TicTacToe.hs
@@ -1,37 +1,42 @@
+{-# OPTIONS_GHC -fdefer-typed-holes #-}
+
 module TicTacToe where
 
-import Data.Map
-import Prelude hiding (lookup)
-import Data.Monoid
-import Data.Maybe (isJust)
-
-data Genome = Genome --test
+import           Data.List   (maximumBy, sortBy)
+import           Data.Map    (Map, empty, insert, lookup, toList)
+import           Data.Maybe  (isJust)
+import           Data.Monoid (First (..), getFirst, (<>))
+import           Data.Ord    (comparing)
+import           Neet
+import           Prelude     hiding (lookup)
 
 type ScoringFunction = Genome -> Genome -> (Double, Double) --TODO move to main module?
 
 data Player = X | O deriving (Enum, Eq, Show)
 
-type Board = Map (Int, Int) Player
+type BoardPos = Int
+type Board    = Map BoardPos Player
 
-move :: Player -> (Int,Int) -> Board -> Maybe Board
-move p xy b | isJust $ lookup xy b = Nothing --se c'è già qualcosa la mossa non è valida
-            | otherwise = Just $ insert xy p b
+move :: Player -> BoardPos -> Board -> Maybe Board
+move p xy b | isJust (lookup xy b) = Nothing
+            | otherwise            = Just (insert xy p b)
 
 getWinner :: Board -> Maybe Player
 getWinner b = getFirst (r0 <> r1 <> r2 <> c0 <> c1 <> c2 <> d0 <> d1)
   where
-    r0 = check (0,0) (1,0) (2,0)
-    r1 = check (0,1) (1,1) (2,1)
-    r2 = check (0,2) (1,2) (2,2)
-    c0 = check (0,0) (0,1) (0,2)
-    c1 = check (1,0) (1,1) (1,2)
-    c2 = check (2,0) (2,1) (2,2)
-    d0 = check (0,0) (1,1) (2,2)
-    d1 = check (2,0) (1,1) (0,2)
-    check xy1 xy2 xy3 | lookup xy1 b == lookup xy2 b && lookup xy1 b == lookup xy3 b = First $ lookup xy1 b
-                      | otherwise = First Nothing
+    r0 = check 0 1 2
+    r1 = check 3 4 5
+    r2 = check 6 7 8
+    c0 = check 0 3 6
+    c1 = check 1 4 7
+    c2 = check 2 5 8
+    d0 = check 0 4 8
+    d1 = check 2 4 6
+    check xy1 xy2 xy3 | lookup xy1 b == lookup xy2 b
+                     && lookup xy1 b == lookup xy3 b = First (lookup xy1 b)
+                      | otherwise                    = First Nothing
 
-play :: (Board -> (Int,Int)) -> (Board -> (Int,Int)) -> (Double, Double)
+play :: (Board -> BoardPos) -> (Board -> BoardPos) -> (Double, Double)
 play = play' X empty
   where
     play' turn board p1 p2 = case winner of Nothing -> scores
@@ -46,11 +51,13 @@ play = play' X empty
         failed X = (-2,0)
         failed O = (0,-2)
 
-genomeToFunctionThatTakesABoardAndReturnsAMove :: Player -> Genome -> (Board -> (Int,Int))
-genomeToFunctionThatTakesABoardAndReturnsAMove = undefined --TODO esecuzione della rete neurale
-
 scoreTicTacToe :: ScoringFunction
-scoreTicTacToe g1 g2 = play
-                         (genomeToFunctionThatTakesABoardAndReturnsAMove X g1)
-                         (genomeToFunctionThatTakesABoardAndReturnsAMove O g2)
+scoreTicTacToe g1 g2 = play (abstractGenome g1 X) (abstractGenome g2 O)
+
+abstractGenome :: Genome -> (Player -> Board -> BoardPos)
+abstractGenome g p board = maximumBy (comparing (outputs !!)) [0..8]
+  where
+    net     = mkPhenotype g
+    inputs  = map (\x -> if snd x == p then 1 else -1) . sortBy (comparing fst) $ toList board
+    outputs = getOutput (snapshot net inputs)
 
