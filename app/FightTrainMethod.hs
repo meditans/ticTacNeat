d70d8cd27dfdec5064e454378b78dff9c4fa274f
meditans <meditans@gmail.com>
2016-04-12 13:39:38 +0200

    Refactoring

--- app/FightTrainMethod.hs
+++ app/FightTrainMethod.hs
@@ -14,7 +14,6 @@ import           Data.Ord              (comparing)
 import           Neet                  (Genome, TrainMethod (..))
 import           System.Random.Shuffle (shuffleM)
 
-
 -- | Dati due genomi, ritorna una coppia di valutazioni della performance
 type ScoringFunction = Genome -> Genome -> (Double, Double)
 
@@ -24,19 +23,31 @@ fightTrainMethod :: (MonadRandom m) => ScoringFunction -> Int -> TrainMethod m
 fightTrainMethod f nRounds = TrainMethod (\tGen -> do
   is <- concat <$> replicateM nRounds (pure     [0 .. length tGen - 1])
   js <- concat <$> replicateM nRounds (shuffleM [0 .. length tGen - 1])
-  let fight i j = let (p1, p2) = f (tGen ^?! element i) (tGen ^?! element j)
-                  in  [(i, p1), (j, p2)]
-      scores = collect . concat $ zipWith fight is js
-  return . updateWith scores $ fmap (const 0) tGen)
+  let scores = collect . concat $ zipWith (partialScore tGen f) is js
+      emptyScores = fmap (const 0) tGen
+  return . updateWith scores $ emptyScores)
 
 ----- Funzioni di implementazione
 
-collect :: (Ord a, Num b) => [(a,b)] -> [(a,b)]
+-- Internamente, usiamo il concetto di PartialScore, che e' una coppia formata
+-- da un indice e una valutazione.
+type PartialScore = (Int, Double)
+
+-- La funzione partialScore crea i due punteggi parziali relativi ad uno scontro,
+-- data la struttura, la funzione di valutazione e i due indici.
+partialScore :: Traversable t => t Genome -> ScoringFunction -> Int -> Int -> [PartialScore]
+partialScore t f i j = [(i, payoff1), (j, payoff2)]
+  where (payoff1, payoff2) = f (t ^?! element i) (t ^?! element j)
+
+-- La funzione collect gestisce una lista di PartialScore eliminando la
+-- duplicazione, ovvero rendendo unico l'indice di riferimento e sommando i
+-- punteggi parziali.
+collect :: [PartialScore] -> [PartialScore]
 collect = map (fst . head &&& sum . map snd)
         . groupBy ((==) `on` fst)
         . sortBy  (comparing fst)
 
-
-updateWith :: (Num a, Traversable t) => [(Int, a)] -> t a -> t a
-updateWith = foldl1' (.)
-           . map (\(i, payoff) -> element i +~ payoff)
+-- La funzione updateWith trasforma una lista di partialscores in una funzione
+-- che modifica i rispettivi elementi della struttura traversabile.
+updateWith :: (Traversable t) => [PartialScore] -> t Double -> t Double
+updateWith = foldl1' (.) . map (\(i, payoff) -> element i +~ payoff)
