c87f864e68912e22c74a6c47ec14a6ad3cfb7f86
meditans <meditans@gmail.com>
2016-04-12 16:30:33 +0200

    Better main, with tracing and best score

--- app/Main.hs
+++ app/Main.hs
@@ -5,17 +5,23 @@
 
 module Main where
 
-import TicTacToe
-import FightTrainMethod
-import Control.Monad.Random
-import Neet
+import           Control.Monad.Random (evalRandIO)
+import qualified Data.ByteString      as BS
+import           Data.Serialize       (encode)
+import           FightTrainMethod     (fightTrainMethod)
+import           Neet
+import           TicTacToe            (scoreTicTacToe)
 
 main :: IO ()
 main = do
-  trainedPop <- evalRandIO $ trainN (fightTrainMethod scoreTicTacToe 3) 1000 ticTacPopulation
-  putStrLn "Here's the best player after this training:"
-  print $ trainedPop
-  printGenome (popBOrg trainedPop)
+  trainedPop <- iterateM 1000 ticTacPopulation (\p -> do
+    putStrLn $! "Evaluating generation " ++ show (popGen p)
+    putStrLn $! "The best score in this generation is " ++ show (popBScore p)
+    p' <- evalRandIO $ trainOnce (fightTrainMethod scoreTicTacToe 3) p
+    return p')
+  putStrLn "Encoding the best genome..."
+  BS.writeFile "genome" (encode $ popBOrg trainedPop)
+  putStrLn "Done"
 
 ticTacPopulation :: Population
 ticTacPopulation = newPop 100 PS { psSize = 100
@@ -26,3 +32,6 @@ ticTacPopulation = newPop 100 PS { psSize = 100
                                  , psStrategy = Nothing
                                  }
 
+iterateM :: Monad m => Int -> a -> (a -> m a) -> m a
+iterateM 0 a _ = return a
+iterateM n a f = f a >>= (\a' -> iterateM (n-1) a' f)
